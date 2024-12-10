3df0d38b02fc2b415248ec9640f75cc77116aadb
adrian-lozan-18 <123754045+adrian-lozan-18@users.noreply.github.com>
2024-12-09 21:47:43 -0600

    Update conexion.php

--- actividad final/php/conexion.php
+++ actividad final/php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'tienda_novelas');
+$conn = mysqli_connect('localhost', 'adrian', 'Adrian_123', 'tienda_novelas');
 if (!$conn) {
     die("Error de conexión: " . mysqli_connect_error());
 }
